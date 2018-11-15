d2f16bbd7ed4aea1d3c12852ba3a122005a87919
Bharvi Bissa <bharvi.b@tadigital.com>
2018-11-15 15:37:46 +0530

    Added Source Code For MavenExampleApplication

---
+++ com.techaspect.mavenexample-0.0.1-SNAPSHOT/src/main/java/com/tadigital/controllers/LinkController.java
@@ -0,0 +1,14 @@
+package com.tadigital.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LinkController {
+	
+	@RequestMapping(value= {"/","/home"})
+	public String index() {
+		return "index";
+	}
+
+}
